892f405c21889b7391fb5d004d35ddda95a413c1
hguruman <hguruman@gmail.com>
2020-12-03 04:22:23 +0400

    Update application file

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,4 +2,11 @@ class MainController < ApplicationController
   def home
     @users = User.all
   end
+
+  def about
+  end
+
+  def contact
+
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < Devise::RegistrationsController
       params.require(:user).permit(:firstname, :lastname, :username, :email, :password, :password_confirmation)
     end
   
-    def update_member_params
+    def update_user_params
       params.require(:user).permit(:firstname, :lastname, :name, :username, :email, :password, :password_confirmation, :current_password)
     end
   end

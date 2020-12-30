94e8e8a2e77b399ece4992c3eb1f649eef96ae53
hguruman <hguruman@gmail.com>
2020-12-30 17:05:23 +0400

    Update post and profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,23 @@ class UsersController < ApplicationController
     # @followed_and_user = current_user.followed_users
   end
 
+  def new
+
+  end
+
+  def edit; end
+
+  # PATCH/PUT /posts/1
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to user_path, notice: 'User Profile was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.order('created_at DESC')
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  validates :postcontent, presence: true, length: { maximum: 140,
-                                                    too_long: '140 characters in post is the maximum allowed.' }
+  validates :postcontent, presence: true, length: { maximum: 500,
+                                                    too_long: '500 characters in post is the maximum allowed.' }
   belongs_to :user, foreign_key: :user_id
   has_many :replies, dependent: :destroy
   has_many :likes, dependent: :destroy
